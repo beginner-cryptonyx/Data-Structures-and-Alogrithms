91d174dd921aab2c8a5a5fd1480f755fbb7680cc
Crypto <mayanksethi242008@gmail.com>
2025-06-28 09:24:52 +0530

    Added all modes of traversal and a search function

--- Data Structures/Binary Search Tree.py
+++ Data Structures/Binary Search Tree.py
@@ -18,29 +18,54 @@ class BinaryTreeNode:
             elif self.left is not None:
                 self.left.insert(value)
 
-    def __inorder(self):
+    def traverse(self, mode="inorder"):
+        if mode == "preorder":
+            print(self.value, end=" ")
         if self.left is not None:
-            self.left.__inorder()
-        print(self.value)
+            self.left.traverse(mode)
+        if mode == "inorder":
+            print(self.value, end=" ")
         if self.right is not None:
-            self.right.__inorder()
+            self.right.traverse(mode)
+        if mode == "postorder":
+            print(self.value, end=" ")
 
-    def traverse(self, mode: str = "inorder"):
-        match mode:
-            case "inorder":
-                self.__inorder()
+    def search(self, element):
+        if self.value == element:
+            return self
+        elif element < self.value and self.left is not None:
+            return self.left.search(element)
+        elif element > self.value and self.right is not None:
+            return self.right.search(element)
 
+    def delete(self, element):
+        if self.search(element) is not None:
+            if self.left == element:
+                if self.left.left is None and self.left.right is None:
+                    self.value = self.left.value
+                    self.left = None
+            # if self.element
+                    
+                
 
 if __name__ == "__main__":
-    root = BinaryTreeNode(20)
-    root.insert(34)
-    root.insert(1)
+    root = BinaryTreeNode(8)
     root.insert(3)
-    root.insert(234)
-    root.insert(323)
-    root.insert(123)
-    root.insert(45)
-    root.insert(26)
+    root.insert(10)
+    root.insert(1)
+    root.insert(6)
+    root.insert(14)
+    root.insert(4)
+    root.insert(7)
+    root.insert(13)
 
+    print("\ninorder")
     root.traverse(mode="inorder")
-    
+
+    print("\npreorder")
+    root.traverse(mode="preorder")
+
+    print("\npostorder")
+    root.traverse(mode="postorder")
+
+    print(root.search(15))
